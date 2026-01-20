87ea3dcf9fc7ed5253393e4d6a256fd42d07f3c7
Mr.Cat <97294806+x11kitty@users.noreply.github.com>
2026-01-20 22:17:40 +0530

    Add files via upload

---
+++ Topic 1/Print.py
@@ -0,0 +1,13 @@
+# This is First Python Program of basic Print Statement Which Display Output On The Console
+# The print() function is a versatile tool for displaying information,
+# including strings, numbers, variables, and the results of
+# expressions. 
+
+# print function is a fundamental command used to output data to
+# the standard output device, typically the console or screen. It
+# allows the user to see the results of a program, such as variable values,
+# strings, or calculated data. 
+
+
+print("This is My First Print Statement")
+print("This is My Second Print Statement Output")
