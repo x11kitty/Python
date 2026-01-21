4066158c873073a875a9832de222f821a2db54fa
Mr.Cat <97294806+x11kitty@users.noreply.github.com>
2026-01-21 13:00:49 +0530

    Add files via upload

---
+++ Topic 2/Variables.py
@@ -0,0 +1,55 @@
+# Variable = A Container for a Value (string, integer, float, boolean)
+#            A Variable Behaves as if it was the Value it Contains
+
+###########
+# Strings # = string is an immutable sequence of Unicode characters used to represent text. They are a fundamental data type, essential for handling text-based data like names, sentences, and messages.
+###########
+
+first_name = "Shanks"
+food = "Chicken"
+email = "Shanks12@gmail.com"
+
+# you can you print(f"{}") Statement which you can add multiple Variable to String
+# print(f"My Name is {first_name} and I Like {food} my email was {email}")
+
+############
+# Integers # = An integer in Python, represented by the int class, is a whole number (positive, negative, or zero) that has no fractional or decimal part.
+############
+
+age = 22
+quantity = 29
+number_of_student = 40
+
+# print(f"My Age is {age} and I have {quantity} quantity of Goods for {number_of_student} Students")
+# or
+# print(age)
+# print(quantity)
+# print(number_of_student)
+
+#########
+# Float # = A float (short for floating-point number) is a standard Python data type used to represent real numbers that have a decimal or fractional component Unlike integers.
+#########
+
+Price = 10.99
+gpa = 4.2
+distance = 5.5
+
+# print(f"The Price is ${Price}")
+# print(f"Your gpa is: {gpa}")
+# print(f"You ran {distance}km")
+
+###########
+# Boolean # = A Boolean in Python is a fundamental data type that represents one of two possible truth values: True or False. It is used for logical operations, conditional statements (like if and while loops), and comparisons to control the flow of a program.
+###########
+
+# Boolean Have only 2 Values true or false Like Yes or No
+
+happy = True
+sad = False
+good = True
+bad = False
+
+print(f"YOu are Happy ? {happy}")
+print(f"You are Sad ? {sad}")
+print(f"Also You are In Good Mood ? {good}")
+print(f"And Not in Bad Mood ? {bad}")
